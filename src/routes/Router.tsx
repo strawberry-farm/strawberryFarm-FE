0bd79c06ec7e6e8207c489e0fdf19c5fc5b05ad0
KooTaehyeon <vcro12123@naver.com>
2023-08-18 21:26:49 +0900

    Chore: 환경세팅

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<>오류수정</>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
