2b70031c50c0bb88ca26e0ebe43ceecd83408d2b
rangggu <aliyah521@naver.com>
2023-08-18 19:40:01 +0900

    Chore : 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Router } from "./routes/Router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Router />;
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
+}
+
+export default App;
